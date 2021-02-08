582aedbc2768ec0bfaf3cd5497d499fdfe91e562
Victor <thenicotain28@gmail.com>
2021-02-08 14:16:37 +0100

    V2.1

--- src/exercise.java
+++ src/exercise.java
@@ -1,9 +1,13 @@
 public class exercise {
+
     public static float determinant(double[] ints, double[] ints1, double[] ints2) {
+
         double cad1 = (ints[0] * ints1[1] * ints2[2]) + (ints[1] * ints1[2] * ints2[0]) + (ints[2] * ints1[0] * ints2[1]);
         double cad2 = (ints2[0] * ints1[1] * ints[2]) + (ints2[1] * ints1[2] * ints[0]) + (ints2[2] * ints1[0] * ints[1]);
         double result = cad1 - cad2;
         float result1 = (float) result;
         return result1;
+
     }
+
 }
